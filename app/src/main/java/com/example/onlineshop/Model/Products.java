30baabafde69f7cf5e059ebdf928a02c8188aeaa
manVsmind <mathurasan1997@gmail.com>
2020-08-17 10:30:00 +0530

    first

---
+++ app/src/main/java/com/example/onlineshop/Model/Products.java
@@ -0,0 +1,36 @@
+package com.example.onlineshop.Model;
+public class Products {
+    private String pname, description, price, image, category, pid, date, time;
+    public Products()
+    {
+
+    }
+    public Products(String pname, String description, String price, String image, String category, String pid, String date, String time) {
+        this.pname = pname;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.category = category;
+        this.pid = pid;
+        this.date = date;
+        this.time = time;
+    }
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }}
